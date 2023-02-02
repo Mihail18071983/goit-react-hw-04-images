5424ed3001b0ffa91f76710c258a62bb7d62f31a
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-02-02 16:29:37 +0200

    Update Searchbar.jsx

--- src/modules/SearchImages/Searchbar/Searchbar.jsx
+++ src/modules/SearchImages/Searchbar/Searchbar.jsx
@@ -1,4 +1,3 @@
-// import { Component } from 'react';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { ImSearch } from 'react-icons/im';
@@ -51,68 +50,6 @@ const Searchbar = ({ onSubmit }) => {
       </header>
     );
 };
-
-// class Searchbar extends Component {
-//   state = {
-//     search: '',
-//   };
-
-//   handleChange = ({ target }) => {
-//     const { name, value } = target;
-//     this.setState({ [name]: value });
-//   };
-
-//   handleSubmit = e => {
-//     const { search } = this.state;
-//     e.preventDefault();
-//     if (search.trim() === '') {
-//       return toast.warn('Enter correct search!');
-//     }
-//     const { onSubmit } = this.props;
-//     onSubmit({ ...this.state });
-//     this.reset();
-//   };
-
-//   reset() {
-//     this.setState({
-//       search: '',
-//     });
-//   }
-
-//   render() {
-//     const { search } = this.state;
-//     const { handleChange, handleSubmit } = this;
-
-//     return (
-//       <header className={styles.Searchbar}>
-//         <form className={styles.SearchForm} onSubmit={handleSubmit}>
-//           <button
-//             type="submit"
-//             className={styles.Button}
-//             aria-label="search-button"
-//           >
-//             <span className={styles.Label}>
-//               <ImSearch />
-//             </span>
-//           </button>
-
-//           <input
-//             name="search"
-//             value={search}
-//             className={styles.Input}
-//             type="text"
-//             autoComplete="off"
-//             autoFocus
-//             placeholder="Search images and photos"
-//             required
-//             onChange={handleChange}
-//           />
-//         </form>
-//       </header>
-//     );
-//   }
-// }
-
 
 export default Searchbar;
 
