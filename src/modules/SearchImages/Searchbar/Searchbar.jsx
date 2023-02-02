3e6fe0531c0eb5b06bd92d984b50c396b11dcb41
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-02-02 10:02:52 +0200

    Update Searchbar.jsx

--- src/modules/SearchImages/Searchbar/Searchbar.jsx
+++ src/modules/SearchImages/Searchbar/Searchbar.jsx
@@ -1,42 +1,30 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { ImSearch } from 'react-icons/im';
 import { toast } from 'react-toastify';
 
 import styles from '../Searchbar/Searchbar.module.css';
 
-class Searchbar extends Component {
-  state = {
-    search: '',
+const Searchbar = ({ onSubmit }) => {
+  const [search, setSearch] = useState('');
+
+  const handleChange = (e) => { 
+    setSearch(e.target.value);
   };
 
-  handleChange = ({ target }) => {
-    const { name, value } = target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
-    const { search } = this.state;
+  const handleSubmit = e => {
     e.preventDefault();
+    console.log(search)
     if (search.trim() === '') {
       return toast.warn('Enter correct search!');
     }
-    const { onSubmit } = this.props;
-    onSubmit({ ...this.state });
-    this.reset();
+
+    onSubmit(search);
+    setSearch('');
   };
 
-  reset() {
-    this.setState({
-      search: '',
-    });
-  }
-
-  render() {
-    const { search } = this.state;
-    const { handleChange, handleSubmit } = this;
-
-    return (
+      return (
       <header className={styles.Searchbar}>
         <form className={styles.SearchForm} onSubmit={handleSubmit}>
           <button
@@ -63,8 +51,68 @@ class Searchbar extends Component {
         </form>
       </header>
     );
-  }
-}
+};
+
+// class Searchbar extends Component {
+//   state = {
+//     search: '',
+//   };
+
+//   handleChange = ({ target }) => {
+//     const { name, value } = target;
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = e => {
+//     const { search } = this.state;
+//     e.preventDefault();
+//     if (search.trim() === '') {
+//       return toast.warn('Enter correct search!');
+//     }
+//     const { onSubmit } = this.props;
+//     onSubmit({ ...this.state });
+//     this.reset();
+//   };
+
+//   reset() {
+//     this.setState({
+//       search: '',
+//     });
+//   }
+
+//   render() {
+//     const { search } = this.state;
+//     const { handleChange, handleSubmit } = this;
+
+//     return (
+//       <header className={styles.Searchbar}>
+//         <form className={styles.SearchForm} onSubmit={handleSubmit}>
+//           <button
+//             type="submit"
+//             className={styles.Button}
+//             aria-label="search-button"
+//           >
+//             <span className={styles.Label}>
+//               <ImSearch />
+//             </span>
+//           </button>
+
+//           <input
+//             name="search"
+//             value={search}
+//             className={styles.Input}
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             required
+//             onChange={handleChange}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
 
 export default Searchbar;
 
